0bc69c903c70ee330e044978685c3884eee6d2cf
qyanamstewart <qyanamstewart@yahoo.com>
2016-09-05 19:49:15 -0400

    add module 5

--- site/module5-solution/script.js
+++ site/module5-solution/script.js
@@ -94,6 +94,8 @@ $ajaxUtils.sendGetRequest(
 // Builds HTML for the home page based on categories array
 // returned from the server.
 function buildAndShowHomeHTML (categories) {
+  document.querySelector("main-content") 
+  .innerHTML = responseText;
 
   // Load home snippet page
   $ajaxUtils.sendGetRequest(
